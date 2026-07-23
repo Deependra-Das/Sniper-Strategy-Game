1c04ef79eb669b8c17dec8a40a211de5d86e30dd
Deependra-Das <iamdeep75@gmail.com>
2026-07-23 12:56:15 +0530

    Updated PlayerController for shooting logic & changes to tag enemy for testing

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using Unity.Cinemachine;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -24,14 +23,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject _scopeOverlay;
     [SerializeField] private float _scopeDuration;
 
+    [Header("Shooting")]
+    [SerializeField] private Bullet _bulletPrefab;
+    [SerializeField] private Transform _bulletSpawnPoint;
+    [SerializeField] private float _range = 100f;
+    [SerializeField] private float _bulletSpeed = 50f;
+    [SerializeField] private LayerMask _hitMask;
+    [SerializeField] private float _boltActionDuration = 1.2f;
+
     private InputAction m_lookAction;
     private InputAction m_scopeAction;
+    private InputAction m_shootAction;
     private Vector2 m_lookAmt;
     private float _yaw;
     private float _pitch;
-    private bool isScoped = false;
+    private bool _isScoped = false;
+    private bool _canShoot = false;
     private float _normalFOV;
-
     private int playerGunLayerMask;
 
     private void OnEnable()
@@ -48,6 +56,7 @@ public class PlayerController : MonoBehaviour
     {
         m_lookAction = InputSystem.actions.FindAction("Look");
         m_scopeAction = InputSystem.actions.FindAction("Scope");
+        m_shootAction = InputSystem.actions.FindAction("Shoot");
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         playerGunLayerMask = 1 << LayerMask.NameToLayer("PlayerGun");
@@ -56,7 +65,8 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         Look();
-        HandleGunInput();
+        HandleGunScopeInput();
+        HandleShootingInput();
     }
 
     private void Look()
@@ -71,14 +81,14 @@ public class PlayerController : MonoBehaviour
         _cameraPivot.localRotation = Quaternion.Euler(_pitch, 0f, 0f);
     }
 
-    private void HandleGunInput()
+    private void HandleGunScopeInput()
     {
         if (m_scopeAction.WasPressedThisFrame())
         {
-            isScoped = !isScoped;
-            _playerGunAnimator.SetBool("isScoped", isScoped);
+            _isScoped = !_isScoped;
+            _playerGunAnimator.SetBool("isScoped", _isScoped);
 
-            if(isScoped)
+            if (_isScoped)
             {
                 StartCoroutine(OnEnterScope());
             }
@@ -96,6 +106,7 @@ public class PlayerController : MonoBehaviour
         _mainCamera.cullingMask &= ~playerGunLayerMask;
         _normalFOV = _playerCamera.Lens.FieldOfView;
         _playerCamera.Lens.FieldOfView = _scopedFOV;
+        _canShoot = true;
     }
 
     private void OnExitScope()
@@ -103,7 +114,56 @@ public class PlayerController : MonoBehaviour
         _scopeOverlay.SetActive(false);
         _mainCamera.cullingMask |= playerGunLayerMask;
         _playerCamera.Lens.FieldOfView = _normalFOV;
+        _canShoot = false;
     }
 
+    private void HandleShootingInput()
+    {
+        if (!_canShoot)
+            return;
 
+        if (!_isScoped)
+            return;
+
+        if (!m_shootAction.WasPressedThisFrame())
+            return;
+
+        StartCoroutine(ShootRoutine());
+    }
+
+    private IEnumerator ShootRoutine()
+    {
+        _canShoot = false;
+
+        yield return new WaitForSeconds(0.05f);
+
+        ShootBullet();
+
+        yield return new WaitForSeconds(_boltActionDuration);
+        _canShoot = true;
+    }
+
+    private void ShootBullet()
+    {
+        Ray ray = new Ray(_mainCamera.transform.position, _mainCamera.transform.forward);
+
+        Vector3 targetPoint;
+
+        if (Physics.Raycast(ray, out RaycastHit hit, _range, _hitMask))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(_range);
+        }
+
+        Vector3 direction = (targetPoint - _bulletSpawnPoint.position);
+
+        Quaternion rotation = Quaternion.LookRotation(direction.normalized) * Quaternion.Euler(90f, 0f, 0f);
+
+        Bullet bullet = Instantiate(_bulletPrefab, _bulletSpawnPoint.position, rotation);
+
+        bullet.Initialize(direction, _bulletSpeed);
+    }
 }
