24f9181d4a9462daa2410520af774683790d7067
Deependra-Das <iamdeep75@gmail.com>
2026-07-28 11:24:51 +0530

    Added GameManager script

---
+++ Assets/Scripts/Main/GameManager.cs
@@ -0,0 +1,13 @@
+using SniperStrategyGame.Utilities;
+using UnityEngine;
+
+namespace SniperStrategyGame.Main
+{
+    public class GameManager : GenericMonoSingleton<GameManager>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+    }
+}
