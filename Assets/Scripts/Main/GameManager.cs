8b274a54169e5c676b126492fe53df1574571767
Deependra-Das <iamdeep75@gmail.com>
2026-07-28 11:26:32 +0530

    Updated GameManager to initialize ServiceLocator

--- Assets/Scripts/Main/GameManager.cs
+++ Assets/Scripts/Main/GameManager.cs
@@ -5,9 +5,17 @@ namespace SniperStrategyGame.Main
 {
     public class GameManager : GenericMonoSingleton<GameManager>
     {
+        public ServiceLocator Services { get; private set; }
+
         protected override void Awake()
         {
             base.Awake();
+            InitializeServices();
+        }
+
+        private void InitializeServices()
+        {
+            Services = new ServiceLocator();
         }
     }
 }
