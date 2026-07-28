fe9bbdc14b18cd227f9813a3078584935b72cd49
Deependra-Das <iamdeep75@gmail.com>
2026-07-28 12:53:53 +0530

    Updated GameManager to load MainMenu Scene

--- Assets/Scripts/Main/GameManager.cs
+++ Assets/Scripts/Main/GameManager.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+using System.Collections;
 using SniperStrategyGame.Utilities;
 using SniperStrategyGame.Event;
 using SniperStrategyGame.SceneLoader;
@@ -6,6 +8,7 @@ namespace SniperStrategyGame.Main
 {
     public class GameManager : GenericMonoSingleton<GameManager>
     {
+        [SerializeField] private float _sceneLoadDelay = 1f;
         public ServiceLocator Services { get; private set; }
         private EventBusService _eventBus;
         private SceneLoaderService _sceneLoader;
@@ -15,6 +18,11 @@ namespace SniperStrategyGame.Main
             base.Awake();
             InitializeServices();
             RegisterServices();
+        }
+
+        private void Start()
+        {
+            StartCoroutine(LoadMainMenuScene());
         }
 
         private void InitializeServices()
@@ -30,5 +38,10 @@ namespace SniperStrategyGame.Main
             Services.Register(_sceneLoader);
         }
 
+        private IEnumerator LoadMainMenuScene()
+        {
+            yield return new WaitForSeconds(_sceneLoadDelay);
+            Services.Get<SceneLoaderService>().LoadScene(SceneNameEnum.MainMenu);
+        }
     }
 }
