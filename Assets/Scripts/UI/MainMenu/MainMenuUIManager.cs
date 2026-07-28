135ad1457cc627987b929eb897478700b15c503d
Deependra-Das <iamdeep75@gmail.com>
2026-07-28 14:49:17 +0530

    Added MAinMenuUIManager and updated MainMenu scene

---
+++ Assets/Scripts/UI/MainMenu/MainMenuUIManager.cs
@@ -0,0 +1,40 @@
+using SniperStrategyGame.Main;
+using SniperStrategyGame.SceneLoader;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUIManager : MonoBehaviour
+{
+    [SerializeField] private Button _playButton;
+    private SceneLoaderService _sceneLoader;
+
+    private void Awake()
+    {
+        _sceneLoader = GameManager.Instance.Services.Get<SceneLoaderService>();
+    }
+
+    private void OnEnable()
+    {
+        SubscribeToEvents();
+    }
+
+    private void OnDisable()
+    {
+        UnsubscribeToEvents();
+    }
+
+    private void SubscribeToEvents()
+    {
+        _playButton.onClick.AddListener(OnPlayButtonClicked);
+    }
+
+    private void UnsubscribeToEvents()
+    {
+        _playButton.onClick.RemoveListener(OnPlayButtonClicked);
+    }
+
+    public void OnPlayButtonClicked()
+    {
+        _sceneLoader.LoadScene(SceneNameEnum.StageSelection);
+    }
+}
