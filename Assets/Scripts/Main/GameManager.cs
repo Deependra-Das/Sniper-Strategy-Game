b50bbebec4456f905a15446890e69c893b93dad5
Deependra-Das <iamdeep75@gmail.com>
2026-07-28 12:22:22 +0530

    Updated GameManager to handle initializing & registering services

--- Assets/Scripts/Main/GameManager.cs
+++ Assets/Scripts/Main/GameManager.cs
@@ -1,23 +1,34 @@
 using SniperStrategyGame.Utilities;
 using SniperStrategyGame.Event;
+using SniperStrategyGame.SceneLoader;
 
 namespace SniperStrategyGame.Main
 {
     public class GameManager : GenericMonoSingleton<GameManager>
     {
         public ServiceLocator Services { get; private set; }
+        private EventBusService _eventBus;
+        private SceneLoaderService _sceneLoader;
 
         protected override void Awake()
         {
             base.Awake();
             InitializeServices();
+            RegisterServices();
         }
 
         private void InitializeServices()
         {
             Services = new ServiceLocator();
-
-            Services.Register(new EventBusService());
+            _eventBus = new EventBusService();
+            _sceneLoader = new SceneLoaderService(_eventBus);
         }
+
+        private void RegisterServices()
+        {
+            Services.Register(_eventBus);
+            Services.Register(_sceneLoader);
+        }
+
     }
 }
