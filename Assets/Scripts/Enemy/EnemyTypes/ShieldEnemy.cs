b6dfcd9c17f4be2ec2c1c7395db691b25352a08e
Deependra-Das <iamdeep75@gmail.com>
2026-07-30 13:10:23 +0530

    Updated ShieldEnemy to handle BulletHit with shield & HandleDeath logic

--- Assets/Scripts/Enemy/EnemyTypes/ShieldEnemy.cs
+++ Assets/Scripts/Enemy/EnemyTypes/ShieldEnemy.cs
@@ -4,9 +4,21 @@ namespace SniperStrategyGame.Enemy
 {
     public class ShieldEnemy : BaseEnemy
     {
+        [SerializeField] private Collider shieldCollider;
+
         protected override void ExecuteBehaviour()
         {
             agent.ResetPath();
+        }
+        public override void OnBulletHit(Collider hitCollider)
+        {
+            if (hitCollider == shieldCollider)
+            {
+                Debug.Log("Shield blocked bullet");
+                return;
+            }
+
+            HandleDeath();
         }
     }
 }
