63d2a1c38db7f23f51329a369b95b68755e5ed84
Deependra-Das <iamdeep75@gmail.com>
2026-08-18 11:32:44 +0530

    Changes to initialize EnemyService

--- Assets/Scripts/Main/GameManager.cs
+++ Assets/Scripts/Main/GameManager.cs
@@ -1,4 +1,5 @@
 using SniperStrategyGame.Bullet;
+using SniperStrategyGame.Enemy;
 using SniperStrategyGame.Event;
 using SniperStrategyGame.SceneLoader;
 using SniperStrategyGame.Utilities;
@@ -11,11 +12,13 @@ namespace SniperStrategyGame.Main
     {
         [SerializeField] private float _sceneLoadDelay = 1f;
         [SerializeField] private PlayerBullet _bulletPrefab;
+        [SerializeField] private Enemy_SO _enemySO;
 
         public ServiceLocator Services { get; private set; }
-        private EventBusService _eventBus;
-        private SceneLoaderService _sceneLoader;
+        private EventBusService _eventBusService;
+        private SceneLoaderService _sceneLoaderService;
         private BulletService _bulletService;
+        private EnemyService _enemyService;
 
         protected override void Awake()
         {
@@ -32,16 +35,18 @@ namespace SniperStrategyGame.Main
         private void InitializeServices()
         {
             Services = new ServiceLocator();
-            _eventBus = new EventBusService();
-            _sceneLoader = new SceneLoaderService(_eventBus);
+            _eventBusService = new EventBusService();
+            _sceneLoaderService = new SceneLoaderService(_eventBusService);
             _bulletService = new BulletService(_bulletPrefab);
+            _enemyService = new EnemyService(_enemySO, _eventBusService);
         }
 
         private void RegisterServices()
         {
-            Services.Register(_eventBus);
-            Services.Register(_sceneLoader);
+            Services.Register(_eventBusService);
+            Services.Register(_sceneLoaderService);
             Services.Register(_bulletService);
+            Services.Register(_enemyService);
         }
 
         private IEnumerator LoadMainMenuScene()
