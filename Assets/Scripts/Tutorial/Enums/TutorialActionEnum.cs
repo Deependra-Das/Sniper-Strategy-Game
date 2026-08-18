f32ee4e73c8dc936e237ec2b079637871d7e8f0d
Deependra-Das <iamdeep75@gmail.com>
2026-08-18 15:28:48 +0530

    Added TutorialActionEnum script

---
+++ Assets/Scripts/Tutorial/Enums/TutorialActionEnum.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace SniperStrategyGame.Tutorial
+{
+    public enum TutorialActionEnum
+    {
+        None,
+        HighlightEnemy,
+        ScopeIn,
+        ScopeOut,
+        Shoot,
+        GuardEnemy,
+        PatrolEnemy,
+        ShieldEnemy,
+    }
+}
