6821e0da36d03154e7c161468a9c503f6662c28b
Deependra-Das <iamdeep75@gmail.com>
2026-09-18 11:18:09 +0530

    Changes in GameplayUIManager & GameplayUIPrefab to handle Mission info Slide In animation

--- Assets/Scripts/UI/Gameplay/GameplayUIManager.cs
+++ Assets/Scripts/UI/Gameplay/GameplayUIManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using SniperStrategyGame.Event;
 using SniperStrategyGame.Main;
+using System.Collections;
 
 namespace SniperStrategyGame.UI
 {
@@ -17,6 +18,15 @@ namespace SniperStrategyGame.UI
         [SerializeField] private GameObject _missionInfoContainer;
         [SerializeField] private TMP_Text _missionNameText;
         [SerializeField] private TMP_Text _missionGoalInfoText;
+        [SerializeField] private GameObject _missionNameContainer;
+        [SerializeField] private CanvasGroup _missionNameContainerCanvasGroup;
+        [SerializeField] private Vector2 _missionNameContainerStartPosition;
+        [SerializeField] private Vector2 _missionNameContainerEndPosition;
+        [SerializeField] private GameObject _missionGoalContainer;
+        [SerializeField] private CanvasGroup _missionGoalContainerCanvasGroup;
+        [SerializeField] private Vector2 _missionGoalContainerStartPosition;
+        [SerializeField] private Vector2 _missionGoalContainerEndPosition;
+        [SerializeField] private float _slideAnimationDuration;
 
 
         private EventBusService _eventBusServiceObj;
@@ -47,6 +57,8 @@ namespace SniperStrategyGame.UI
 
         private void Start()
         {
+            ToggleMissionNameContainer(false);
+            ToggleMissionGoalContainer(false);
             ShowScopeIn();
         }
 
@@ -93,6 +105,76 @@ namespace SniperStrategyGame.UI
         {
             SetMissionNameText(eventObj.MissionName);
             SetMissionGoalInfoText(eventObj.MissionGoal);
+            StartCoroutine(ShowMissionInfo());
+        }
+
+        private IEnumerator ShowMissionInfo()
+        {
+            StartCoroutine(AnimateSlideFadeUI(_missionNameContainer, _missionNameContainerCanvasGroup,
+                _missionNameContainerStartPosition, _missionNameContainerEndPosition, FadeTypeEnum.FadeIn, _slideAnimationDuration));
+
+            yield return new WaitForSeconds(_slideAnimationDuration);
+
+            StartCoroutine(AnimateSlideFadeUI(_missionGoalContainer, _missionGoalContainerCanvasGroup,
+                _missionGoalContainerStartPosition, _missionGoalContainerEndPosition, FadeTypeEnum.FadeIn, _slideAnimationDuration));
+        }
+
+        private void HideMissionInfo()
+        {
+            StartCoroutine(AnimateSlideFadeUI(_missionNameContainer, _missionNameContainerCanvasGroup,
+                _missionNameContainerEndPosition, _missionNameContainerStartPosition, FadeTypeEnum.FadeOut, _slideAnimationDuration));
+        }
+
+        private IEnumerator AnimateSlideFadeUI(GameObject gameObj, CanvasGroup canvasGroup, Vector2 startPosition, Vector2 endPosition, FadeTypeEnum fadeType, float duration)
+        {
+            if (gameObj == null || canvasGroup == null)
+                yield break;
+
+            RectTransform rectTransform = gameObj.GetComponent<RectTransform>();
+
+            if (rectTransform == null)
+                yield break;
+
+            if (fadeType == FadeTypeEnum.FadeIn)
+                gameObj.SetActive(true);
+
+            float startAlpha = fadeType == FadeTypeEnum.FadeIn ? 0f : 1f;
+            float endAlpha = fadeType == FadeTypeEnum.FadeIn ? 1f : 0f;
+
+            rectTransform.anchoredPosition = startPosition;
+            canvasGroup.alpha = startAlpha;
+
+            float elapsed = 0f;
+
+            while (elapsed < duration)
+            {
+                elapsed += Time.deltaTime;
+
+                float timer = Mathf.Clamp01(elapsed / duration);
+                timer = Mathf.SmoothStep(0f, 1f, timer);
+
+                rectTransform.anchoredPosition = Vector2.Lerp(startPosition, endPosition, timer);
+
+                canvasGroup.alpha = Mathf.Lerp(startAlpha, endAlpha, timer);
+
+                yield return null;
+            }
+
+            rectTransform.anchoredPosition = endPosition;
+            canvasGroup.alpha = endAlpha;
+
+            if (fadeType == FadeTypeEnum.FadeOut)
+                gameObj.SetActive(false);
+        }
+
+        private void ToggleMissionNameContainer(bool value)
+        {
+            _missionNameContainer.SetActive(value);
+        }
+
+        private void ToggleMissionGoalContainer(bool value)
+        {
+            _missionGoalContainer.SetActive(value);
         }
     }
 }
